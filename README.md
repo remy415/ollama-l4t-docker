a173bbf8c829f2701390b2927a59c4f9c1b1e3a2
remy415 <remy415@gmail.com>
2024-03-26 16:02:43 +0000

    Add dockerfile & build scripts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# docker-ollama
+# Ollama Tegra Fix
+## Jetson support should be included as of Ollama 0.1.30. Please see the official repo for more information https://github.com/ollama/ollama
+
+### ENSURE THESE TWO VARIABLES ARE SET:
+`export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/local/cuda/compat:/usr/local/cuda/include"`
+`export OLLAMA_SKIP_CPU_GENERATE="1"`
+
+## Also, Tegra devices will fail `go generate ./...` unless architectures are set:
+### L4T_VERSION.major >= 36:    # JetPack 6
+`export CMAKE_CUDA_ARCHITECTURES="87"`
+### L4T_VERSION.major >= 34:  # JetPack 5
+`export CMAKE_CUDA_ARCHITECTURES="72;87"`
+### L4T_VERSION.major == 32:  # JetPack 4
+`export CMAKE_CUDA_ARCHITECTURES="53;62;72"`
---
+++ docker/scripts/tegra_linux_deps.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Script for common Dockerfile dependency installation in Tegra L4T based images
+
+set -ex
+MACHINE=$(uname -m)
+
+if grep -i "ubuntu" /etc/os-release >/dev/null; then
+    # Tegra L4T 35.x images are Ubuntu 20.04-based, L4T 36.x images are Ubuntu 22.04-based
+
+    if [ "${MACHINE}" = "aarch64" ]; then
+        add-apt-repository ppa:git-core/ppa
+        add-apt-repository ppa:ubuntu-toolchain-r/test
+        apt update && apt -y install git-all gcc-10 g++-10
+    fi
+else
+    echo "ERROR Unexpected distro"
+    exit 1
+fi
+
+if [ -n "${CMAKE_VERSION}" ]; then
+    curl -s -L https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-$(uname -m).tar.gz | tar -zx -C /usr --strip-components 1
+    rm /usr/local/bin/cmake && update-alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake 30
+fi
+
+if [ -n "${GOLANG_VERSION}" ]; then
+    GO_ARCH="arm64"
+    mkdir -p /usr/local
+    curl -s -L https://dl.google.com/go/go${GOLANG_VERSION}.linux-${GO_ARCH}.tar.gz | tar xz -C /usr/local
+    ln -s /usr/local/go/bin/go /usr/local/bin/go
+    ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
+fi
