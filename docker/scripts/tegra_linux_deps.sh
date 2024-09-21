ebbf99c6f658b000bde47ef1ce8a39da05ca2a40
Jeremy <remy415@gmail.com>
2024-09-20 21:03:09 -0400

    Update tegra_linux_deps.sh
    Adding proxy support

--- docker/scripts/tegra_linux_deps.sh
+++ docker/scripts/tegra_linux_deps.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Script for common Dockerfile dependency installation in Tegra L4T based images
-
+CURL_OPTIONS="-s -L"
+if [ -n "JETSON_DBUILD_PROXY" ]; then
+    CURL_OPTIONS="${CURL_OPTIONS} -x ${JETSON_DBUILD_PROXY}"
+fi
 set -ex
 MACHINE=$(uname -m)
 
@@ -19,14 +22,14 @@ else
 fi
 
 if [ -n "${CMAKE_VERSION}" ]; then
-    curl -s -L https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-$(uname -m).tar.gz | tar -zx -C /usr --strip-components 1
+    curl ${CURL_OPTIONS} https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-$(uname -m).tar.gz | tar -zx -C /usr --strip-components 1
     rm /usr/local/bin/cmake && update-alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake 30
 fi
 
 if [ -n "${GOLANG_VERSION}" ]; then
     GO_ARCH="arm64"
     mkdir -p /usr/local
-    curl -s -L https://dl.google.com/go/go${GOLANG_VERSION}.linux-${GO_ARCH}.tar.gz | tar xz -C /usr/local
+    curl ${CURL_OPTIONS} https://dl.google.com/go/go${GOLANG_VERSION}.linux-${GO_ARCH}.tar.gz | tar xz -C /usr/local
     ln -s /usr/local/go/bin/go /usr/local/bin/go
     ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
 fi
